f3ffa301d0f3e5fe1cd3e2dd76a51c8b3425cdc3
0-yash-sharma-0 <yasharma999999@gmail.com>
2023-12-24 00:55:29 +0530

    Day 3 : Completed Login page UI

--- lib/features/authentication/controllers/onboarding_controller.dart
+++ lib/features/authentication/controllers/onboarding_controller.dart
@@ -1,3 +1,4 @@
+import 'package:e_store/features/authentication/screens/login/login.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -21,6 +22,7 @@ class OnBoardingController extends GetxController {
 
   void nextPage() {
     if (currentPageIndex?.value == 2) {
+      Get.offAll(const LoginScreen());
     } else {
       int page = currentPageIndex!.value + 1;
       pageController.jumpToPage(page);
--- lib/features/authentication/screens/login/login.dart
+++ lib/features/authentication/screens/login/login.dart
@@ -1,19 +1,193 @@
+import 'package:e_store/common/styles/spacing_styles.dart';
+import 'package:e_store/utils/constants/colors.dart';
+import 'package:e_store/utils/constants/image_strings.dart';
 import 'package:e_store/utils/constants/sizes.dart';
+import 'package:e_store/utils/constants/text_strings.dart';
+import 'package:e_store/utils/helpers/helper_functions.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:iconsax/iconsax.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    final dark = EHelperFunctions.isDarkMode(context);
+    return Scaffold(
       body: SingleChildScrollView(
         child: Padding(
-            padding: EdgeInsets.only(
-                top: ESizes.appBarHeight,
-                left: ESizes.defaultSpace,
-                bottom: ESizes.defaultSpace,
-                right: ESizes.defaultSpace)),
+          padding: ESpacingStyle.paddingWithAppBarHeight,
+          child: Column(
+            children: [
+              /// Logo , title & Sub-title
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Image(
+                      height: 150,
+                      image: AssetImage(
+                          dark ? EImages.lightAppLogo : EImages.darkAppLogo)),
+                  Text(
+                    ETexts.loginTitle,
+                    style: Theme.of(context).textTheme.headlineMedium,
+                  ),
+                  const SizedBox(
+                    height: ESizes.sm,
+                  ),
+                  Text(
+                    ETexts.loginSubTitle,
+                    style: Theme.of(context).textTheme.bodyMedium,
+                  )
+                ],
+              ),
+
+              /// FORM
+
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                    vertical: ESizes.spaceBtwSections),
+                child: Form(
+                  child: Column(
+                    children: [
+                      ///Email
+                      TextFormField(
+                        decoration: const InputDecoration(
+                            prefixIcon: Icon(Iconsax.direct_right),
+                            labelText: ETexts.email),
+                      ),
+                      const SizedBox(
+                        height: ESizes.spaceBtwInputFields,
+                      ),
+
+                      /// Password
+                      TextFormField(
+                        decoration: const InputDecoration(
+                          prefixIcon: Icon(Iconsax.password_check),
+                          labelText: ETexts.password,
+                          suffixIcon: Icon(Iconsax.eye_slash),
+                        ),
+                      ),
+
+                      const SizedBox(
+                        height: ESizes.spaceBtwInputFields / 2,
+                      ),
+                      // Remember Me & forget password
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          // Remember me
+                          Row(
+                            children: [
+                              Checkbox(value: true, onChanged: (value) {}),
+                              const Text(ETexts.rememberMe),
+                            ],
+                          ),
+
+                          //Forget Password
+                          TextButton(
+                              onPressed: () {},
+                              child: const Text(ETexts.forgetPassword)),
+                        ],
+                      ),
+                      const SizedBox(
+                        height: ESizes.spaceBtwSections,
+                      ),
+
+                      //Sign In Button
+                      SizedBox(
+                          width: double.infinity,
+                          child: ElevatedButton(
+                              onPressed: () {},
+                              child: const Text(ETexts.signIn))),
+
+                      const SizedBox(
+                        height: ESizes.spaceBtwItems,
+                      ),
+
+                      /// Create Account Button
+
+                      SizedBox(
+                        width: double.infinity,
+                        child: OutlinedButton(
+                            onPressed: () {},
+                            child: const Text(ETexts.createAccount)),
+                      ),
+                      const SizedBox(
+                        height: ESizes.spaceBtwSections,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+
+              /// Divider
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Flexible(
+                    child: Divider(
+                      color: dark ? EColors.darkGrey : EColors.grey,
+                      thickness: 0.5,
+                      indent: 60,
+                      endIndent: 5,
+                    ),
+                  ),
+                  Text(
+                    ETexts.orSignInWith.capitalize!,
+                    style: Theme.of(context).textTheme.labelMedium,
+                  ),
+                  Flexible(
+                    child: Divider(
+                      color: dark ? EColors.darkGrey : EColors.grey,
+                      thickness: 0.5,
+                      indent: 5,
+                      endIndent: 60,
+                    ),
+                  ),
+                ],
+              ),
+
+              const SizedBox(height: ESizes.spaceBtwSections),
+
+              ///Footer
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(color: EColors.grey),
+                      borderRadius: BorderRadius.circular(100),
+                    ),
+                    child: IconButton(
+                        onPressed: () {},
+                        icon: const Image(
+                          image: AssetImage(EImages.google),
+                          width: ESizes.iconMd,
+                          height: ESizes.iconMd,
+                        )),
+                  ),
+                  const SizedBox(
+                    width: ESizes.spaceBtwItems,
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(color: EColors.grey),
+                      borderRadius: BorderRadius.circular(100),
+                    ),
+                    child: IconButton(
+                        onPressed: () {},
+                        icon: const Image(
+                          image: AssetImage(EImages.facebook),
+                          width: ESizes.iconMd,
+                          height: ESizes.iconMd,
+                        )),
+                  )
+                ],
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
